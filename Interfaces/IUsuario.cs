dece55e3a841d3ce7b3b0e1dedd85d8b22b3fc36
Felipe Tadeu Neves Santos <40270522859@info.local>
2018-11-22 17:10:23 -0200

    Primeiro Commit Felipe

---
+++ Interfaces/IUsuario.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using Senai.Financas.Mvc.Web.Models;
+
+namespace Senai_Financas_Web_Mvc_Tarde_master.Interfaces
+{
+    public interface IUsuario
+    {
+         UsuarioModel Cadastrar(UsuarioModel usuario);
+         List<UsuarioModel> Listar();
+         void Excluir(int id);
+
+         UsuarioModel Editar(UsuarioModel usuario);
+         UsuarioModel BuscarPorEmailESenha(string email, string senha);
+         UsuarioModel BuscarPorId(int id);
+    }
+}
